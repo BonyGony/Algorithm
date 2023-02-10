e8c63411060861039d47153a7b45338fe3c1b1be
Bony <gubonhy@naver.com>
2023-02-10 13:31:04 +0900

    chapter6_3 실전문제3 정렬

---
+++ Book/chapter6_3.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
+
+a_arr.sort()
+b_arr.sort(reverse=True)
+
+for i in range(k):
+    if a_arr[i] < b_arr[i]:
+        a_arr[i] = b_arr[i]
+    else:
+        break
+
+print(sum(a_arr))
