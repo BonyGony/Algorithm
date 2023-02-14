68e4beb65f0e432d564ac7e825625c8175329b58
Bony <gubonhy@naver.com>
2023-02-14 19:40:00 +0900

    chapter4_2 구현문제2 구현

---
+++ Book/chapter4_2.py
@@ -0,0 +1,11 @@
+cnt = 0
+
+n = int(input())
+
+for hour in range(n+1):
+    for minute in range(60):
+        for second in range(60):
+            if '3' in str(hour) or '3' in str(minute) or '3' in str(second):
+                cnt += 1
+
+print(cnt)
